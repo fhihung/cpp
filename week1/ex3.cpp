3246cd353ee5e10d4d37be0ee5bddffa4f066c7f
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-02-09 07:40:23 +0700

    test

---
+++ week1/ex3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float h, r;
+    cout << "Nhap chieu hinh tru: ";
+    cin >> h;
+    cout << "Nhap ban kinh day: ";
+    cin >> r;
+    cout << "The tich hinh tru la: " << 3.14*r*r*h;
+    
+
+}
