d095d055432452966ff3971dbdfece46dd613f9a
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-02-10 14:31:22 +0700

    sss

--- week3/ex2.cpp
+++ week3/ex2.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    string name = " HelloWorld";
-    for (char &c : name){
-        if (islower(c)){
-            c = toupper(c);
-            
+    // string name = " HelloWorld";
+    // for (char &c : name){
+    //     if (islower(c)){
+    //         c = toupper(c);
+    //     }
+    // cout << name << endl;
+    // }
+    int n = 3;
+    string ten = "HelloWorld";
+    for (int i = ten.size() - 1; i >= ten.size() -n ; --i){
+            char &c = ten[i];
+            if(islower(c)){
+                c = toupper(c);
+                cout << c << endl;
+            }
         }
-    cout << c << endl;
-    }
+
+    cout << ten << endl;
     return 0;
 }
